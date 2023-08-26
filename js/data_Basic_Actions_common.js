4d2123c5db83ec39418d5de46e53935dce66427e
Intervencion <janraven55@gmail.com>
2023-08-26 21:07:43 +0200

    Icon transition

--- js/data_Basic_Actions_common.js
+++ js/data_Basic_Actions_common.js
@@ -1,6 +1,20 @@
 data_movement = [
     {
-        title: "Move",
+        title: "Stride",
+        icon: "FreeAction",
+        subtitle: "Cost: 5ft per 5ft",
+        description: "Movement cost: 5ft per 5ft moved",
+        reference: "PHB, pg. 190.",
+        bullets: [
+            "If you have more than one speed, such as your walking speed and a flying speed, you can switch back and forth between your speeds during your move. Whenever you switch, subtract the distance you've already moved from the new speed.",
+            "You can move through a nonhostile creature's space.",
+            "You can move through a hostile creature's space only if the creature is at least two sizes larger or smaller than you.",
+            "Another creature's space is difficult terrain for you.",
+            "Whether a creature is a friend or an enemy, you can't willingly end your move in its space."
+        ]
+    },
+    {
+        title: "Strike",
         icon: "walk",
         subtitle: "Cost: 5ft per 5ft",
         description: "Movement cost: 5ft per 5ft moved",
@@ -14,7 +28,49 @@ data_movement = [
         ]
     },
     {
-        title: "Drop prone",
+        title: "Cast a spell",
+        icon: "walk",
+        subtitle: "Cost: 5ft per 5ft",
+        description: "Movement cost: 5ft per 5ft moved",
+        reference: "PHB, pg. 190.",
+        bullets: [
+            "If you have more than one speed, such as your walking speed and a flying speed, you can switch back and forth between your speeds during your move. Whenever you switch, subtract the distance you've already moved from the new speed.",
+            "You can move through a nonhostile creature's space.",
+            "You can move through a hostile creature's space only if the creature is at least two sizes larger or smaller than you.",
+            "Another creature's space is difficult terrain for you.",
+            "Whether a creature is a friend or an enemy, you can't willingly end your move in its space."
+        ]
+    },
+    {
+        title: "Step",
+        icon: "walk",
+        subtitle: "Cost: 5ft per 5ft",
+        description: "Movement cost: 5ft per 5ft moved",
+        reference: "PHB, pg. 190.",
+        bullets: [
+            "If you have more than one speed, such as your walking speed and a flying speed, you can switch back and forth between your speeds during your move. Whenever you switch, subtract the distance you've already moved from the new speed.",
+            "You can move through a nonhostile creature's space.",
+            "You can move through a hostile creature's space only if the creature is at least two sizes larger or smaller than you.",
+            "Another creature's space is difficult terrain for you.",
+            "Whether a creature is a friend or an enemy, you can't willingly end your move in its space."
+        ]
+    },
+    {
+        title: "Raise a shield",
+        icon: "walk",
+        subtitle: "Cost: 5ft per 5ft",
+        description: "Movement cost: 5ft per 5ft moved",
+        reference: "PHB, pg. 190.",
+        bullets: [
+            "If you have more than one speed, such as your walking speed and a flying speed, you can switch back and forth between your speeds during your move. Whenever you switch, subtract the distance you've already moved from the new speed.",
+            "You can move through a nonhostile creature's space.",
+            "You can move through a hostile creature's space only if the creature is at least two sizes larger or smaller than you.",
+            "Another creature's space is difficult terrain for you.",
+            "Whether a creature is a friend or an enemy, you can't willingly end your move in its space."
+        ]
+    },
+    {
+        title: "Stride",
         icon: "despair",
         subtitle: "Cost: 0ft",
         description: "Movement cost: 0ft (free)",
